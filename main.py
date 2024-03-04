f510abc8f7063b066d3e09e1cc1f32beb4d9fad5
Oksana <96268693+CryptoOks@users.noreply.github.com>
2024-03-04 20:25:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+class Point:
+    def init(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if type(x) in (int, float):
+            self.__x = x
+        else:
+            print("Value must be numeric.")
+            self.__x = None
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if type(y) in (int, float):
+            self.__y = y
+        else:
+            print("Value must be numeric.")
+            self.__y = None
